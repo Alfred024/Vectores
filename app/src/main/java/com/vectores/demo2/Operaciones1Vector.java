39cae3dc721ac57cf8b6d4e6c5e13705b6ca7a25
Alfred024 <21030761tcelaya.edu.mx>
2023-01-23 09:44:03 -0600

    Setted up the methods of activity Operaciones1Vector

--- app/src/main/java/com/vectores/demo2/Operaciones1Vector.java
+++ app/src/main/java/com/vectores/demo2/Operaciones1Vector.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -29,10 +30,83 @@ public class Operaciones1Vector extends AppCompatActivity {
         operationView = (TextView)findViewById(R.id.operationLabelB);
 
         spinner1 = (Spinner)findViewById(R.id.mainSpinnerB);
-        String operaciones[] = {"Longitud vector", "Cosenos directores", "Multiplicaciónde vector por x"};
+        String operaciones[] = {"Longitud vector", "Cosenos directores", "Multiplicación de vector por x"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item_operaciones, operaciones);
         spinner1.setAdapter(adapter);
     }
 
+    Vector getVector(){
+        String xA = x.getText().toString();
+        if(xA.isEmpty()){ xA = "0";}
+        String yA = y.getText().toString();
+        if(yA.isEmpty()){ yA = "0";}
+        String zA = z.getText().toString();
+        if(zA.isEmpty()){ zA = "0";}
 
+        int xNum = Integer.parseInt(xA);
+        int yNum = Integer.parseInt(yA);
+        int zNum = Integer.parseInt(zA);
+        Vector vector = new Vector(xNum, yNum, zNum);
+        return vector;
+    }
+
+    boolean vectorVacio(EditText x, EditText y, EditText z){
+        return x.getText().toString().isEmpty() && y.getText().toString().isEmpty() && z.getText().toString().isEmpty();
+    }
+
+    public void calcularButton(View view){
+        Vector vector1 = getVector();
+        String operacion = spinner1.getSelectedItem().toString();
+
+        if(operacion.equals("Longitud vector")){
+            operationView.setText("Longitud (módulo) de vector");
+        }
+        if(operacion.equals("Cosenos directores")){
+            operationView.setText("Cosenos directores");
+
+        }
+        if(operacion.equals("Multiplicación de vector por x")){
+            operationView.setText("Multiplicación de vector por x");
+        }
+
+    }
+
+    public String getProcedimientoView(Vector v1, Vector v2, String operacion){
+        Procedimientos objProcedimientos = new Procedimientos(v1);
+        char signoOperacion = ' ', signoOperacion2;
+        String procedimiento, procedimiento2="";
+        Vector res = null;
+
+        double resUnidad;
+        if(operacion.equals("Longitud vector")){
+            signoOperacion = '*';
+            res = v1.sumaVectores(v2);
+        }
+        if(operacion.equals("Cosenos directores")){
+            signoOperacion = '-';
+            res = v1.restaVectores(v2);
+        }
+        if(operacion.equals("Multiplicación de vector por x")){
+            signoOperacion = '·';
+            res = v1.multiVector(v2);
+        }
+        procedimiento = objProcedimientos.getProcedimiento(v2, res, signoOperacion)+procedimiento2;
+        return procedimiento;
+    }
+
+    public void magnitud(Vector v1, Vector v2){
+        Vector resultante = v1.sumaVectores(v2);
+        String resSuma = v1.showRes(resultante);
+        resView.setText(resSuma);
+    }
+    public void cosenosDirs(Vector v1, Vector v2){
+        Vector resultante = v1.restaVectores(v2);
+        String resResta = v1.showRes(resultante);
+        resView.setText(resResta);
+    }
+    public void multiplicaion(Vector v1, Vector v2){
+        Vector resultante = v1.multiVector(v2);
+        String resMulti = "";
+        resView.setText(resMulti);
+    }
 }
