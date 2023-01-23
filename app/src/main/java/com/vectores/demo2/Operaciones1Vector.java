6721e2be3abc81d15edc39cb8f527522e6abffac
Alfred024 <21030761tcelaya.edu.mx>
2023-01-22 23:03:26 -0600

    View operacion1Vector creada

---
+++ app/src/main/java/com/vectores/demo2/Operaciones1Vector.java
@@ -0,0 +1,18 @@
+package com.vectores.demo2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Operaciones1Vector extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operaciones1_vector);
+
+
+    }
+}
