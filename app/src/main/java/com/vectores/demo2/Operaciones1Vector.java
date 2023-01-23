ad91204d6b805efa67528cba00eda1efd88828ae
Alfred024 <21030761tcelaya.edu.mx>
2023-01-23 08:45:47 -0600

    OPeraciones1Vector layouts setted up

--- app/src/main/java/com/vectores/demo2/Operaciones1Vector.java
+++ app/src/main/java/com/vectores/demo2/Operaciones1Vector.java
@@ -1,9 +1,11 @@
 package com.vectores.demo2;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 public class Operaciones1Vector extends AppCompatActivity {
 
@@ -12,7 +14,5 @@ public class Operaciones1Vector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operaciones1_vector);
-
-
     }
 }
